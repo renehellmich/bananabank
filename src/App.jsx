d94c1dd6d35430eb568709f51b32436515418d09
renehellmich <renehellmich94@gmail.com>
2024-01-19 13:17:09 +0100

    stage 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react'
+import Main from './assets/components/main/main'
 import './App.css'
 
 function App() {
-  
+
+  const [obj, setObj] = useState({
+    credit: 0,
+    total: 0,
+  })
 
   return (
     <>
-      
+      <body>
+        <header className='header'>
+          <h1 id='title'>Banana Bank</h1>
+        </header>
+        <main>
+          <Main
+            obj={obj}
+            setObj={setObj}
+          />
+        </main>
+      </body>
     </>
   )
 }
---
+++ src/assets/components/main/main.jsx
@@ -0,0 +1,52 @@
+const Main = ({obj, setObj}) => {
+
+    const amountUp = () => {
+        console.log(typeof obj, typeof setObj);
+        obj.total = document.querySelector('#geldbetrag').value
+        console.log(typeof Number(obj.total), obj.total);
+        if (typeof Number(obj.total) === "number") {
+            setObj({ ...obj, credit: obj.credit + Math.round(Number(obj.total) * 100) / 100})
+        } 
+    }
+
+    const amountDown = () => {
+        console.log(typeof obj, typeof setObj);
+        obj.total = document.querySelector('#geldbetrag').value
+        console.log(typeof Number(obj.total), obj.total);
+        if (typeof Number(obj.total) === "number") {
+            setObj({ ...obj, credit: obj.credit - Math.round(Number(obj.total) * 100) / 100})
+        }  
+    }
+
+    return ( 
+        <>
+            <div className="konto">
+                <h2>Mein Girokonto</h2>
+                <p className="saldo">{obj.credit}</p>
+                <input type="number" 
+                name="geldbetrag"
+                id="geldbetrag"
+                className="geldbetrag" 
+                placeholder="Betrag in €"
+                value={obj.title}
+                />
+                <div>
+                    <button
+                    onClick={amountUp}
+                    className="einzahlen"
+                    >
+                        Einzahlen
+                    </button>
+                    <button
+                    onClick={amountDown}
+                    className="auszahlen"
+                    >
+                        Auszahlen
+                    </button>
+                </div>
+            </div>
+        </>
+     );
+}
+ 
+export default Main;
